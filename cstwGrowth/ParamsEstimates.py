f964ec8a9c86fe558133ab8507c197ed526b0137
arotare1 <32108515+arotare1@users.noreply.github.com>
2018-03-25 21:23:23 -0400

    Store estimated parameters in this file

---
+++ cstwGrowth/ParamsEstimates.py
@@ -0,0 +1,13 @@
+'''
+Contains the estimates for center and spread obtained under the default parameters
+of each specification. Can be loaded into cstwGrowth.py directly to save time
+'''
+
+# BetaPointPYnw (estimates obtained using PerGroFac_i == [1.0])
+center_BetaPointPYnw = 0.989139523424
+spread_BetaPointPYnw = 0.0
+
+# BetaPointLCnw (estimates obtained using PerGroFacAgg == 1.015**0.25)
+center_BetaPointLCnw = 0.989050218236
+spread_BetaPointLCnw = 0.0
+
