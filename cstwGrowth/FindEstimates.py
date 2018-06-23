323690079ea7421b5847922bc178ae03996a4c62
arotare1 <32108515+arotare1@users.noreply.github.com>
2018-06-23 18:08:58 -0400

    Clean code, get rid of lifecycle and aggregate shock specifications

--- cstwGrowth/FindEstimates.py
+++ cstwGrowth/FindEstimates.py
@@ -1,31 +1,35 @@
 '''
-Finds estimates for the center and spread of the beta distribution and saves them in ./ParamsEstimates/
+Finds estimates for the center and spread of the beta distribution for 2014 US data.
+
+Reads wealth data from ../output/CountryWealth/
+
+Saves output in ../output/BaselineEstimates/
 '''
 
 import pdb
 import numpy as np
-from copy import copy, deepcopy
 from time import clock
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import pickle
 import pandas as pd
 from scipy.optimize import golden, brentq
-from HARKutilities import approxMeanOneLognormal, approxUniform, getPercentiles, getLorenzShares
 import SetupParams as Params
-from cstwGrowth import cstwMPCagent, cstwMPCmarket, calcStationaryAgeDstn, \
-                        findLorenzDistanceAtTargetKY, getKYratioDifference
+from cstwGrowth import cstwMPCagent, cstwMPCmarket, findLorenzDistanceAtTargetKY, getKYratioDifference
 
 
 Params.do_param_dist = False     # Do param-dist version if True, param-point if False
-Params.do_lifecycle = False     # Use lifecycle model if True, perpetual youth if False
-estimation_growth = 1.0         # Set growth rate to be used when estimating parameters 
-                                # 1.0 for Baseline, >1 for HighEstimationGrowth
-path_estimation_growth = 'Baseline/' if estimation_growth == 1 else 'HighEstimationGrowth/'
+do_actual_KY = False            # Use actual K/Y ratio from WID.world if True, 10.26 o.w.
+estimation_growth = 1.015**(0.25)     # Set growth rate to be used when estimating parameters 
+                            # If equal to 1 estimates are saved in ../output/BaselineEstimates/NoGrowth/
+                            # If > 1 estimates are saved in ../output/BaselineEstimates/HighGrowth
+#path_estimation_growth = 'NoGrowth/' if estimation_growth == 1 else ''
+
+pdb.set_trace()
 
 # Update spec_name
-Params.spec_name = 'Dist' if Params.do_param_dist else 'Point'
-Params.spec_name += 'LC' if Params.do_lifecycle else 'PY'
+Params.spec_name = '/NoGrowth' if estimation_growth == 1 else '/HighGrowth'
+Params.spec_name += '/ActualKY' if do_actual_KY else '/BaselineKY' 
+Params.spec_name += '/Dist' if Params.do_param_dist else '/Point'
 
 # Set number of beta types
 if Params.do_param_dist:
@@ -33,82 +37,45 @@ if Params.do_param_dist:
 else:
     Params.pref_type_count = 1       # Just one beta type in beta-point
     
-# Set targets for K/Y and the Lorenz curve based on the data
-if Params.do_liquid:
-    lorenz_target = np.array([0.0, 0.004, 0.025,0.117])
-    KY_target = 6.60
-else: # This is hacky until I can find the liquid wealth data and import it
-    lorenz_target = getLorenzShares(Params.SCF_wealth,weights=Params.SCF_weights,percentiles=Params.percentiles_to_match)
-    lorenz_long_data = np.hstack((np.array(0.0),getLorenzShares(Params.SCF_wealth,weights=Params.SCF_weights,percentiles=np.arange(0.01,1.0,0.01).tolist()),np.array(1.0)))
-    #lorenz_target = np.array([-0.002, 0.01, 0.053,0.171])
+# Set targets for K/Y and the Lorenz curve based on 2015 US data
+path_to_lorenz = '../../output/CountryWealth/WealthData_US_20.csv'
+lorenz_long_data = pd.read_csv(path_to_lorenz)['botsh_after'].values
+lorenz_long_data = np.hstack((np.array(0.0), lorenz_long_data, np.array(1.0)))
+lorenz_target = lorenz_long_data[np.array([int(100*p) for p in Params.percentiles_to_match])]
+if do_actual_KY:
+    KY_target = pd.read_csv(path_to_lorenz)['KY_after'].values[0]
+else:
     KY_target = 10.26
 
 # Make AgentTypes for estimation
-if Params.do_lifecycle:
-    DropoutType = cstwMPCagent(**Params.init_dropout)
-    DropoutType.PermGroFac = [estimation_growth]    # Update growth factor
-    DropoutType.AgeDstn = calcStationaryAgeDstn(DropoutType.LivPrb,True)
-    HighschoolType = deepcopy(DropoutType)
-    HighschoolType(**Params.adj_highschool)
-    HighschoolType.AgeDstn = calcStationaryAgeDstn(HighschoolType.LivPrb,True)
-    CollegeType = deepcopy(DropoutType)
-    CollegeType(**Params.adj_college)
-    CollegeType.AgeDstn = calcStationaryAgeDstn(CollegeType.LivPrb,True)
-    DropoutType.update()
-    HighschoolType.update()
-    CollegeType.update()
-    EstimationAgentList = []
-    for n in range(Params.pref_type_count):
-        EstimationAgentList.append(deepcopy(DropoutType))
-        EstimationAgentList.append(deepcopy(HighschoolType))
-        EstimationAgentList.append(deepcopy(CollegeType))
-else:
-    if Params.do_agg_shocks:
-        PerpetualYouthType = cstwMPCagent(**Params.init_agg_shocks)
-        PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
-    else:
-        PerpetualYouthType = cstwMPCagent(**Params.init_infinite)
-        PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
-    PerpetualYouthType.AgeDstn = np.array(1.0)
-    EstimationAgentList = []
-    for n in range(Params.pref_type_count):
-        EstimationAgentList.append(deepcopy(PerpetualYouthType))
+PerpetualYouthType = cstwMPCagent(**Params.init_infinite)
+PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
+PerpetualYouthType.AgeDstn = np.array(1.0)
+EstimationAgentList = []
+for n in range(Params.pref_type_count):
+    EstimationAgentList.append(deepcopy(PerpetualYouthType))
         
+
 # Give all the AgentTypes different seeds
 for j in range(len(EstimationAgentList)):
     EstimationAgentList[j].seed = j
     
+
 # Make an economy for the consumers to live in
 EstimationEconomy = cstwMPCmarket(**Params.init_market)
 if Params.do_param_dist:    # Update simulation parameters
-    if Params.do_agg_shocks:
-        EstimationEconomy.Population = 16800
-    else:
-        EstimationEconomy.Population = 14000
+    EstimationEconomy.Population = 14000
 else:
-    if Params.do_agg_shocks:
-        EstimationEconomy.Population = 9600
-    else:
-        EstimationEconomy.Population = 10000    # Total number of simulated agents in the population
+    EstimationEconomy.Population = 10000    # Total number of simulated agents in the population
 EstimationEconomy.agents = EstimationAgentList
 EstimationEconomy.KYratioTarget = KY_target
 EstimationEconomy.LorenzTarget = lorenz_target
 EstimationEconomy.LorenzData = lorenz_long_data
-if Params.do_lifecycle:
-    EstimationEconomy.PopGroFac = Params.PopGroFac
-    EstimationEconomy.TypeWeight = Params.TypeWeight_lifecycle
-    EstimationEconomy.T_retire = Params.working_T-1
-    EstimationEconomy.act_T = Params.T_sim_LC
-    EstimationEconomy.ignore_periods = Params.ignore_periods_LC
-else:
-    EstimationEconomy.PopGroFac = 1.0
-    EstimationEconomy.TypeWeight = [1.0]
-    EstimationEconomy.act_T = Params.T_sim_PY
-    EstimationEconomy.ignore_periods = Params.ignore_periods_PY
-if Params.do_agg_shocks:
-    EstimationEconomy(**Params.aggregate_params)
-    EstimationEconomy.update()
-    EstimationEconomy.makeAggShkHist()
+EstimationEconomy.PopGroFac = 1.0
+EstimationEconomy.TypeWeight = [1.0]
+EstimationEconomy.act_T = Params.T_sim_PY
+EstimationEconomy.ignore_periods = Params.ignore_periods_PY
+
     
 # Find estimates of center and spread
     
@@ -117,15 +84,8 @@ if Params.param_name == 'CRRA':
     param_range = [0.2,70.0]
     spread_range = [0.00001,1.0]
 elif Params.param_name == 'DiscFac':
-    if estimation_growth == 1.0:
-        param_range = [0.95,0.995]
-        spread_range = [0.006,0.008]
-    else:
-        param_range = [0.95,0.998]
-        spread_range = [0.006,0.008]
-        
-else:
-    print('Parameter range for ' + Params.param_name + ' has not been defined!')
+    param_range = [0.95,0.995]
+    spread_range = [0.006,0.008]     
 
 if Params.do_param_dist:
     # Run the param-dist estimation
@@ -154,34 +114,58 @@ else:
 
 print('Estimate is center=' + str(center_estimate) + ', spread=' + str(spread_estimate) + ', took ' + str(t_end-t_start) + ' seconds.')
 
+
+# Solve the economy for the estimated parameters
+EstimationEconomy.LorenzBool = True
+EstimationEconomy.ManyStatsBool = True
+EstimationEconomy.center_estimate = center_estimate
+EstimationEconomy.spread_estimate = spread_estimate
+EstimationEconomy.distributeParams(Params.param_name,Params.pref_type_count,center_estimate,
+                                   spread_estimate,Params.dist_type)
+EstimationEconomy.solve()
+EstimationEconomy.calcLorenzDistance()
+EstimationEconomy.showManyStats()
+
+
+# Make figure of Lorenz fit
+LorenzAxis = np.arange(101,dtype=float)
+fig = plt.figure()
+plt.plot(LorenzAxis, EstimationEconomy.LorenzData, '-k', linewidth=1.5, label = 'data')
+plt.plot(LorenzAxis, EstimationEconomy.LorenzLongLvlSim, '--', label = 'model')
+plt.xlabel('Wealth percentile',fontsize=12)
+plt.ylabel('Cumulative wealth share',fontsize=12)
+plt.ylim([-0.04,1.0])
+plt.legend(loc='upper left')
+plt.show()
+fig.savefig('../../output/BaselineEstimates' + Params.spec_name + '.pdf')
+
 # Save estimates and a bunch of parameters used in estimation
-with open('./ParamsEstimates/' + path_estimation_growth + Params.spec_name + '.pkl', 'w') as f:
-    pickle.dump([center_estimate,
-                 spread_estimate, 
-                 EstimationEconomy.agents[0].PermGroFac[0],
-                 EstimationEconomy.agents[0].T_age,
-                 EstimationEconomy.agents[0].Rfree,
-                 EstimationEconomy.agents[0].CRRA], f)
-with open('./ParamsEstimates/' + path_estimation_growth + Params.spec_name + '.txt','w') as f:
+with open('../../output/BaselineEstimates' + Params.spec_name + '.pkl', 'w') as f:
+    pickle.dump([center_estimate, spread_estimate], f)
+with open('../../output/BaselineEstimates' + Params.spec_name + '.txt','w') as f:
     f.write('center_estimate = %s \
             \nspread_estimate = %s \
             \ngrowth factor used for estimation is %s \
             \nT_age used for estimation is %s \
             \nRfree used for estimation is %s \
-            \nCRRA is %s'
+            \nCRRA used for estimation is %s \
+            \nK/Y ratio to match is %s \
+            \nLorenz percentiles to match are %s \
+            \nLorenz distance is %s \
+            \nSeconds it took to estimate: %s'
             % (center_estimate,
                spread_estimate,
                EstimationEconomy.agents[0].PermGroFac[0],
                EstimationEconomy.agents[0].T_age,
                EstimationEconomy.agents[0].Rfree,
-               EstimationEconomy.agents[0].CRRA))
+               EstimationEconomy.agents[0].CRRA,
+               EstimationEconomy.KYratioTarget,
+               EstimationEconomy.LorenzTarget,
+               EstimationEconomy.LorenzDistance,
+               t_end-t_start))
     
-# Save EstimationEconomy as .pkl
-with open('./ParamsEstimates/' + path_estimation_growth + Params.spec_name + '_EstimationEconomy.pkl', 'wb') as f:
+# Save EstimationEconomy
+with open('../../output/BaselineEstimates' + Params.spec_name + '_EstimationEconomy.pkl', 'wb') as f:
     pickle.dump(EstimationEconomy, f, pickle.HIGHEST_PROTOCOL)
 
-
-
-
-    
     
