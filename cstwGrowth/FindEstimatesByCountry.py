a7fd2954d05457989d667fcbc5b08aade91235d6
arotare1 <32108515+arotare1@users.noreply.github.com>
2018-06-24 00:22:29 -0400

    Clean code

--- cstwGrowth/FindEstimatesByCountry.py
+++ cstwGrowth/FindEstimatesByCountry.py
@@ -2,9 +2,9 @@
 For each of the countries ES, FR, GB, US, finds estimates for center and spread to match the 
 wealth distribution in 1988 using the growth rate that was observed over the 25-year period 1963-1988.
 
-Assumes the wealth data for each country exists in ../../output/countryWealth/
+Assumes the wealth data for each country exists in ../../output/CountryWealth/
 
-Saves estimates in ../../output/countryEstimates/
+Saves estimates in ../../output/CountryEstimates/
 '''
 
 import pdb
@@ -19,50 +19,28 @@ import SetupParams as Params
 from cstwGrowth import cstwMPCagent, cstwMPCmarket, calcStationaryAgeDstn, \
                         findLorenzDistanceAtTargetKY, getKYratioDifference
                         
-Params.do_param_dist = True     # Do param-dist version if True, param-point if False
-Params.do_lifecycle = False     # Use lifecycle model if True, perpetual youth if False
-
+Params.do_param_dist = False     # Do param-dist version if True, param-point if False
+horizon = '20'      # Set interval over which we analyze changes in the wealth distribution
+                    # Can be 20, 25, 30
 do_more_targets = False  # Set percentiles_to_match=[0.1,0.2,..,0.9] instead of [0.2,0.4,0.6,0.8] if True
 do_actual_KY = True      # Set K/Y ratio from data instead of 10.26 if True
 do_low_T_age = True      # Set the maximum age in simulation to 200 (=74 yrs) intead of 400 if True
 do_high_Rfree = False    # Set quarterly interest rate to 1.02 instead of 1.01 if True
-do_high_CRRA = True     # Set CRRA coefficient to be 1.25 instead of 1 if True
-do_baseline = not do_actual_KY and not do_more_targets and not do_low_T_age \
-                and not do_high_Rfree and not do_high_CRRA
+do_high_CRRA = False     # Set CRRA coefficient to be 2.0 instead of 1 if True
 
-Params.spec_name = '/lower_T_age_actual_KY_high_CRRA/'
-## Update spec_name
-#if do_baseline:
-#    Params.spec_name = '/baseline/'
-#
-#if do_more_targets:
-#    Params.percentiles_to_match = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-#    if do_actual_KY and not do_low_T_age:
-#        Params.spec_name = '/more_targets/actual_KY/'
-#    if do_low_T_age and not do_actual_KY:
-#        Params.spec_name = '/more_targets/low_T_age/'
-#    if do_low_T_age and do_actual_KY:
-#        if do_high_CRRA:
-#            Params.spec_name = '/more_targets/low_T_age_actual_KY_high_CRRA/'
-#        else:
-#            Params.spec_name = '/more_targets/low_T_age_actual_KY/'
-#    
-#else:
-#    if do_actual_KY and not do_low_T_age:
-#        Params.spec_name = '/actual_KY/'
-#    if do_low_T_age and not do_actual_KY:
-#        Params.spec_name = '/low_T_age/'
-#    if do_low_T_age and do_actual_KY:
-#        if do_high_CRRA:
-#            Params.spec_name = '/low_T_age_actual_KY_high_CRRA/'
-#        else:
-#            Params.spec_name = '/low_T_age_actual_KY/'
-#    
-#if do_high_Rfree:
-#    Params.spec_name = '/high_Rfree/'
-    
-Params.spec_name += 'Dist' if Params.do_param_dist else 'Point'
-Params.spec_name += 'LC' if Params.do_lifecycle else 'PY'
+
+# Update spec_name
+if do_more_targets:
+    Params.spec_name += '/more_targets'
+    Params.percentiles_to_match = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+if do_actual_KY and not do_low_T_age:
+    Params.spec_name += '/actual_KY'
+if do_low_T_age and not do_actual_KY:
+    Params.spec_name += '/low_T_age'
+if do_low_T_age and do_actual_KY:
+    Params.spec_name += '/low_T_age_actual_KY'
+Params.spec_name += '/Dist' if Params.do_param_dist else '/Point'
+Params.spec_name += '_' + horizon
 
 # Set number of beta types
 if Params.do_param_dist:
@@ -70,14 +48,17 @@ if Params.do_param_dist:
 else:
     Params.pref_type_count = 1       # Just one beta type in beta-point
 
-#country_list = ['ES', 'FR', 'GB', 'US']
-country_list = ['FR', 'US']
+pdb.set_trace()
+
+# Find estimates for each country
+country_list = ['ES', 'FR', 'GB', 'US']
+#country_list = ['US']
 
 for country in country_list:
     print('Now finding estimates for ' + country + '\n')
     
     # Read Lorenz curve and growth to be used in estimation from the corresponding .csv file
-    path_to_lorenz = '../../output/countryWealth/wealthData_' + country + '.csv'
+    path_to_lorenz = '../../output/CountryWealth/WealthData_' + country + '_' + horizon + '.csv'
     lorenz_long_data = pd.read_csv(path_to_lorenz)['botsh_now'].values
     lorenz_long_data = np.hstack((np.array(0.0), lorenz_long_data, np.array(1.0)))
     lorenz_target = lorenz_long_data[np.array([int(100*p) for p in Params.percentiles_to_match])]
@@ -85,85 +66,43 @@ for country in country_list:
         KY_target = pd.read_csv(path_to_lorenz)['KY_now'].values[0]
     else:
         KY_target = 10.26
-    estimation_growth = pd.read_csv(path_to_lorenz)['growth_before_penn'].values[0]**0.25
+    estimation_growth = pd.read_csv(path_to_lorenz)['growth_before_wb'].values[0]**0.25
+    estimation_year = pd.read_csv(path_to_lorenz)['now'].values[0]
     
     # Make AgentTypes for estimation
-    if Params.do_lifecycle:
-        DropoutType = cstwMPCagent(**Params.init_dropout)
-        DropoutType.PermGroFac = [estimation_growth*g for g in DropoutType.PermGroFac]
-        DropoutType.PermGroFacAgg = 1.0      # Turn off technological growth
-        DropoutType.AgeDstn = calcStationaryAgeDstn(DropoutType.LivPrb,True)
-        HighschoolType = deepcopy(DropoutType)
-        HighschoolType(**Params.adj_highschool)
-        HighschoolType.PermGroFac = [estimation_growth*g for g in HighschoolType.PermGroFac]
-        HighschoolType.AgeDstn = calcStationaryAgeDstn(HighschoolType.LivPrb,True)
-        CollegeType = deepcopy(DropoutType)
-        CollegeType(**Params.adj_college)
-        CollegeType.PermGroFac = [estimation_growth*g for g in CollegeType.PermGroFac]
-        CollegeType.AgeDstn = calcStationaryAgeDstn(CollegeType.LivPrb,True)
-        DropoutType.update()
-        HighschoolType.update()
-        CollegeType.update()
-        EstimationAgentList = []
-        for n in range(Params.pref_type_count):
-            EstimationAgentList.append(deepcopy(DropoutType))
-            EstimationAgentList.append(deepcopy(HighschoolType))
-            EstimationAgentList.append(deepcopy(CollegeType))
-    else:
-        if Params.do_agg_shocks:
-            PerpetualYouthType = cstwMPCagent(**Params.init_agg_shocks)
-            PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
-        else:
-            PerpetualYouthType = cstwMPCagent(**Params.init_infinite)
-            PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
-            if do_low_T_age:
-                PerpetualYouthType.T_age = 184      # Update T_age
-            if do_high_Rfree:
-                PerpetualYouthType.Rfree = 1.02/Params.LivPrb_i[0] # Update Rfree
-            if do_high_CRRA:
-                PerpetualYouthType.CRRA = 1.95
-        PerpetualYouthType.AgeDstn = np.array(1.0)
-        EstimationAgentList = []
-        for n in range(Params.pref_type_count):
-            EstimationAgentList.append(deepcopy(PerpetualYouthType))
+    PerpetualYouthType = cstwMPCagent(**Params.init_infinite)
+    PerpetualYouthType.PermGroFac = [estimation_growth]     # Update growth factor
+    if do_low_T_age:
+        PerpetualYouthType.T_age = 200      # Update T_age
+    if do_high_Rfree:
+        PerpetualYouthType.Rfree = 1.02/Params.LivPrb_i[0] # Update Rfree
+    if do_high_CRRA:
+        PerpetualYouthType.CRRA = 2.0       # Update CRRA
+    PerpetualYouthType.AgeDstn = np.array(1.0)
+    EstimationAgentList = []
+    for n in range(Params.pref_type_count):
+        EstimationAgentList.append(deepcopy(PerpetualYouthType))
             
     # Give all the AgentTypes different seeds
     for j in range(len(EstimationAgentList)):
-        EstimationAgentList[j].seed = j
-    #pdb.set_trace()    
+        EstimationAgentList[j].seed = j    
     
     # Make an economy for the consumers to live in
     EstimationEconomy = cstwMPCmarket(**Params.init_market)
     EstimationEconomy.LorenzPercentiles = Params.percentiles_to_match # Update percentiles to match
     if Params.do_param_dist:    # Update simulation parameters
-        if Params.do_agg_shocks:
-            EstimationEconomy.Population = 16800
-        else:
-            EstimationEconomy.Population = 14000
+        EstimationEconomy.Population = 14000
     else:
-        if Params.do_agg_shocks:
-            EstimationEconomy.Population = 9600
-        else:
-            EstimationEconomy.Population = 10000    # Total number of simulated agents in the population
+        EstimationEconomy.Population = 10000    # Total number of simulated agents in the population
     EstimationEconomy.agents = EstimationAgentList
     EstimationEconomy.KYratioTarget = KY_target
     EstimationEconomy.LorenzTarget = lorenz_target
     EstimationEconomy.LorenzData = lorenz_long_data
-    if Params.do_lifecycle:
-        EstimationEconomy.PopGroFac = Params.PopGroFac
-        EstimationEconomy.TypeWeight = Params.TypeWeight_lifecycle
-        EstimationEconomy.T_retire = Params.working_T-1
-        EstimationEconomy.act_T = Params.T_sim_LC
-        EstimationEconomy.ignore_periods = Params.ignore_periods_LC
-    else:
-        EstimationEconomy.PopGroFac = 1.0
-        EstimationEconomy.TypeWeight = [1.0]
-        EstimationEconomy.act_T = Params.T_sim_PY
-        EstimationEconomy.ignore_periods = Params.ignore_periods_PY
-    if Params.do_agg_shocks:
-        EstimationEconomy(**Params.aggregate_params)
-        EstimationEconomy.update()
-        EstimationEconomy.makeAggShkHist()
+    EstimationEconomy.PopGroFac = 1.0
+    EstimationEconomy.TypeWeight = [1.0]
+    EstimationEconomy.act_T = Params.T_sim_PY
+    EstimationEconomy.ignore_periods = Params.ignore_periods_PY
+    
         
     # Find estimates of center and spread
         
@@ -191,7 +130,6 @@ for country in country_list:
                                        center = 0.999,
                                        spread = 0.006,
                                        dist_type = Params.dist_type)
-    #pdb.set_trace()
     if x*y > 0:
         print('Estimation failed for ' + country + '\n')
     else:
@@ -238,31 +176,31 @@ for country in country_list:
         LorenzAxis = np.arange(101,dtype=float)
         if country == 'ES':     # Must deal with missing values
             fig = plt.figure()
-            plt.plot(LorenzAxis, EstimationEconomy.LorenzData.astype(np.double), '.k', label = country+' data')
+            plt.plot(LorenzAxis, EstimationEconomy.LorenzData.astype(np.double), '.k', label = 'data')
             plt.plot(LorenzAxis, EstimationEconomy.LorenzLongLvlSim, '--', label = 'model')
-            plt.xlabel('Wealth percentile',fontsize=12)
-            plt.ylabel('Cumulative wealth share',fontsize=12)
-            plt.title(country + ' wealth distribution 1988')
-            plt.ylim([-0.02,1.0])
+            plt.xlabel('Wealth percentile')
+            plt.ylabel('Cumulative wealth share')
+            plt.title(country + ' wealth distribution ' + str(estimation_year))
+            plt.ylim([-0.04,1.0])
             plt.legend(loc='upper left')
             plt.show()
-            fig.savefig('../../output/countryEstimates_longer_horizon/' + country + Params.spec_name + '.pdf')
+            fig.savefig('../../output/CountryEstimates/' + country + Params.spec_name + '.pdf')
         else:
             fig = plt.figure()
-            plt.plot(LorenzAxis, EstimationEconomy.LorenzData, '-k', linewidth=1.5, label = country+' data')
+            plt.plot(LorenzAxis, EstimationEconomy.LorenzData, '-k', linewidth = 1.5, label = 'data')
             plt.plot(LorenzAxis, EstimationEconomy.LorenzLongLvlSim, '--', label = 'model')
-            plt.xlabel('Wealth percentile',fontsize=12)
-            plt.ylabel('Cumulative wealth share',fontsize=12)
-            plt.title(country + ' wealth distribution 1988')
-            plt.ylim([-0.02,1.0])
+            plt.xlabel('Wealth percentile')
+            plt.ylabel('Cumulative wealth share')
+            plt.title(country + ' wealth distribution ' + str(estimation_year))
+            plt.ylim([-0.04,1.0])
             plt.legend(loc='upper left')
             plt.show()
-            fig.savefig('../../output/countryEstimates_longer_horizon/' + country + Params.spec_name + '.pdf')
+            fig.savefig('../../output/CountryEstimates/' + country + Params.spec_name + '.pdf')
         
         # Save estimates and a bunch of parameters used in estimation
-        with open('../../output/countryEstimates_longer_horizon/' + country + Params.spec_name + '.pkl', 'w') as f:
+        with open('../../output/CountryEstimates/' + country + Params.spec_name + '.pkl', 'w') as f:
             pickle.dump([center_estimate, spread_estimate], f)
-        with open('../../output/countryEstimates_longer_horizon/' + country + Params.spec_name + '.txt','w') as f:
+        with open('../../output/CountryEstimates/' + country + Params.spec_name + '.txt','w') as f:
             f.write('center_estimate = %s \
                     \nspread_estimate = %s \
                     \ngrowth factor used for estimation is %s \
@@ -285,20 +223,6 @@ for country in country_list:
                        t_end-t_start))
             
         # Save EstimationEconomy
-        with open('../../output/countryEstimates_longer_horizon/' + country + Params.spec_name + '_EstimationEconomy.pkl', 'wb') as f:
+        with open('../../output/CountryEstimates/' + country + Params.spec_name + '_EstimationEconomy.pkl', 'wb') as f:
             pickle.dump(EstimationEconomy, f, pickle.HIGHEST_PROTOCOL)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
