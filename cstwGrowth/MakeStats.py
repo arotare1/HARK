2854d25cbd17683cb14508019d9c8645985c015e
arotare1 <32108515+arotare1@users.noreply.github.com>
2018-06-25 16:17:45 -0400

    Solve model for different combinations of T_age and growth factors. Save output as csv to be read into R and plotted against data.

--- cstwGrowth/MakeStats.py
+++ cstwGrowth/MakeStats.py
@@ -1,5 +1,8 @@
 '''
-Plots inequality measures for different growth factors and saves them in ../output/Figures_model/
+Takes simulation results from VaryGrowth.py and computes various inequality stats for different
+combinations of growth factors and terminal ages.
+
+Saves inequality measures as .csv in ../../output/VaryGrowth/
 
 Assumes that estimates of center and spread have already been computed and stored in 
 ../../output/BaselineEstimates/
@@ -20,7 +23,7 @@ from cstwGrowth import getGiniPrc
 Params.do_param_dist = True     # Do param-dist version if True, param-point if False
 do_actual_KY = True            # Use actual K/Y ratio from WID.world if True, 10.26 o.w.
 do_low_T_age = True      # Set the maximum age in simulation to 200 (=74 yrs) intead of 400 if True
-estimation_growth = 1.0     # Set growth rate to be used when estimating parameters 
+estimation_growth = 1.015**(0.25)     # Set growth rate to be used when estimating parameters 
                             # If equal to 1 estimates are saved in ../output/BaselineEstimates/NoGrowth/
                             # If > 1 estimates are saved in ../output/BaselineEstimates/HighGrowth
 
@@ -42,6 +45,7 @@ with open('../../output/BaselineEstimates' + Params.spec_name + '_EstimationEcon
 
 # Load inequality stats
 with open('../../output/VaryGrowth/' + Params.spec_name + '.pkl') as f:
+    T_ageSim,\
     annual_growthFactors,\
     growthFactors,\
     LorenzLongLvlSim,\
@@ -89,7 +93,8 @@ for i in range(len(aLvl_top1_share)):
     
     
 # Save stats on wealth levels to .csv    
-csvdict = {'annual_growth': annual_growthFactors,
+csvdict = {'T_age' : T_ageSim,
+           'annual_growth': annual_growthFactors,
            'gini' : aLvlGiniSim,
            'mean_to_median' : aLvlMeanToMedianSim,
            'top1_share' : aLvl_top1_share,
@@ -103,67 +108,67 @@ csvdict = {'annual_growth': annual_growthFactors,
 df = pd.DataFrame.from_dict(csvdict)
 df.to_csv('../../output/VaryGrowth/' + Params.spec_name + '.csv')
     
-# Plot Gini coefficient
-fig = plt.figure()
-plt.plot(annual_growthFactors, aLvlGiniSim, '-ro', label='wealth levels')
-plt.plot(annual_growthFactors, aNrmGiniSim, '-go', label='wealth ratios')
-plt.plot(annual_growthFactors, IncGiniSim, '-bo', label='income')
-plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
-plt.xlabel('Annual growth')
-plt.title('Gini coefficient')
-plt.legend(loc='upper right')
-plt.show()
-fig.savefig('../../output/Figures_model' + Params.spec_name + '_gini.pdf')
-
-
-# Plot mean-to-median ratio
-fig = plt.figure()
-plt.plot(annual_growthFactors, aLvlMeanToMedianSim, '-ro', label='wealth levels')
-plt.plot(annual_growthFactors, aNrmMeanToMedianSim, '-go', label='wealth ratios')
-plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
-plt.xlabel('Annual growth')
-plt.title('Mean to median ratio')
-plt.legend(loc='upper right')
-plt.show()
-fig.savefig('../../output/Figures_model' + Params.spec_name + '_mean_to_median.pdf')
-
-
-# Plot top wealth shares (levels)
-fig = plt.figure()
-plt.plot(annual_growthFactors, aLvl_top1_share, '-ro', label='top 1%')
-plt.plot(annual_growthFactors, aLvl_top5_share, '-go', label='top 5%')
-plt.plot(annual_growthFactors, aLvl_top10_share, '-bo', label='top 10%')
-plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
-plt.title('Top wealth shares (levels)')
-plt.xlabel('Annual growth')
-plt.legend(loc='upper right')
-plt.show()
-fig.savefig('../../output/Figures_model' + Params.spec_name + '_top_shares_levels.pdf')
-
-
-# Plot top wealth shares (ratios)
-fig = plt.figure()
-plt.plot(annual_growthFactors, aNrm_top1_share, '-ro', label='top 1%')
-plt.plot(annual_growthFactors, aNrm_top5_share, '-go', label='top 5%')
-plt.plot(annual_growthFactors, aNrm_top10_share, '-bo', label='top 10%')
-plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
-plt.title('Top wealth shares (ratios)')
-plt.xlabel('Annual growth')
-plt.legend(loc='upper right')
-plt.show()
-fig.savefig('../../output/Figures_model' + Params.spec_name + '_top_shares_ratios.pdf')
-
-
-# Plot absolute deviations from median
-fig = plt.figure()
-plt.plot(annual_growthFactors, aLvl_top1_to_median, '-ro', label='top 1%')
-plt.plot(annual_growthFactors, aLvl_top5_to_median, '-go', label='top 5%')
-plt.plot(annual_growthFactors, aLvl_top10_to_median, '-bo', label='top 10%')
-plt.plot(annual_growthFactors, aLvl_bot40_to_median, '-yo', label='bottom 40%')
-plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
-plt.title('Average wealth relative to median')
-plt.xlabel('Annual growth')
-plt.legend(loc='upper right')
-plt.show()
-fig.savefig('../../output/Figures_model' + Params.spec_name + '_dev_from_median.pdf')
-
+## Plot Gini coefficient
+#fig = plt.figure()
+#plt.plot(annual_growthFactors, aLvlGiniSim, '-ro', label='wealth levels')
+#plt.plot(annual_growthFactors, aNrmGiniSim, '-go', label='wealth ratios')
+#plt.plot(annual_growthFactors, IncGiniSim, '-bo', label='income')
+#plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
+#plt.xlabel('Annual growth')
+#plt.title('Gini coefficient')
+#plt.legend(loc='upper right')
+#plt.show()
+#fig.savefig('../../output/Figures_model' + Params.spec_name + '_gini.pdf')
+#
+#
+## Plot mean-to-median ratio
+#fig = plt.figure()
+#plt.plot(annual_growthFactors, aLvlMeanToMedianSim, '-ro', label='wealth levels')
+#plt.plot(annual_growthFactors, aNrmMeanToMedianSim, '-go', label='wealth ratios')
+#plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
+#plt.xlabel('Annual growth')
+#plt.title('Mean to median ratio')
+#plt.legend(loc='upper right')
+#plt.show()
+#fig.savefig('../../output/Figures_model' + Params.spec_name + '_mean_to_median.pdf')
+#
+#
+## Plot top wealth shares (levels)
+#fig = plt.figure()
+#plt.plot(annual_growthFactors, aLvl_top1_share, '-ro', label='top 1%')
+#plt.plot(annual_growthFactors, aLvl_top5_share, '-go', label='top 5%')
+#plt.plot(annual_growthFactors, aLvl_top10_share, '-bo', label='top 10%')
+#plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
+#plt.title('Top wealth shares (levels)')
+#plt.xlabel('Annual growth')
+#plt.legend(loc='upper right')
+#plt.show()
+#fig.savefig('../../output/Figures_model' + Params.spec_name + '_top_shares_levels.pdf')
+#
+#
+## Plot top wealth shares (ratios)
+#fig = plt.figure()
+#plt.plot(annual_growthFactors, aNrm_top1_share, '-ro', label='top 1%')
+#plt.plot(annual_growthFactors, aNrm_top5_share, '-go', label='top 5%')
+#plt.plot(annual_growthFactors, aNrm_top10_share, '-bo', label='top 10%')
+#plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
+#plt.title('Top wealth shares (ratios)')
+#plt.xlabel('Annual growth')
+#plt.legend(loc='upper right')
+#plt.show()
+#fig.savefig('../../output/Figures_model' + Params.spec_name + '_top_shares_ratios.pdf')
+#
+#
+## Plot absolute deviations from median
+#fig = plt.figure()
+#plt.plot(annual_growthFactors, aLvl_top1_to_median, '-ro', label='top 1%')
+#plt.plot(annual_growthFactors, aLvl_top5_to_median, '-go', label='top 5%')
+#plt.plot(annual_growthFactors, aLvl_top10_to_median, '-bo', label='top 10%')
+#plt.plot(annual_growthFactors, aLvl_bot40_to_median, '-yo', label='bottom 40%')
+#plt.axvline(x = Economy.agents[0].PermGroFac[0]**4)
+#plt.title('Average wealth relative to median')
+#plt.xlabel('Annual growth')
+#plt.legend(loc='upper right')
+#plt.show()
+#fig.savefig('../../output/Figures_model' + Params.spec_name + '_dev_from_median.pdf')
+#
--- cstwGrowth/VaryGrowth.py
+++ cstwGrowth/VaryGrowth.py
@@ -1,5 +1,5 @@
 '''
-Solves model for different growth factors and saves results in ../../output/VaryGrowth/
+Solves model for different growth factors and terminal ages and saves results in ../../output/VaryGrowth/
 
 Assumes that estimates of center and spread have already been computed and stored 
 in ../../output/BaselineEstimates/
@@ -19,11 +19,9 @@ from cstwGrowth import getGiniPrc
 Params.do_param_dist = True     # Do param-dist version if True, param-point if False
 do_actual_KY = True            # Use actual K/Y ratio from WID.world if True, 10.26 o.w.
 do_low_T_age = True      # Set the maximum age in simulation to 200 (=74 yrs) intead of 400 if True
-estimation_growth = 1.0     # Set growth rate to be used when estimating parameters 
+estimation_growth = 1.015**(0.25)     # Set growth rate to be used when estimating parameters 
                             # If equal to 1 estimates are saved in ../output/BaselineEstimates/NoGrowth/
                             # If > 1 estimates are saved in ../output/BaselineEstimates/HighGrowth
-
-#pdb.set_trace()
 
 # Update spec_name
 Params.spec_name = '/NoGrowth' if estimation_growth == 1 else '/HighGrowth'
@@ -44,6 +42,7 @@ with open('../../output/BaselineEstimates' + Params.spec_name + '_EstimationEcon
 # Solve the model for different growth factors and save results
 annual_growthFactors = np.arange(1.0, 1.07, 0.01)
 growthFactors = np.power(annual_growthFactors, 0.25)
+T_ageSim = []
 LorenzLongLvlSim = []
 LorenzLongNrmSim = []
 LorenzLongIncSim = []
@@ -59,43 +58,51 @@ aNrmMeanToMedianSim = []
 aLvlPercentilesSim = []
 aNrmPercentilesSim = []
 
-for i in range(len(growthFactors)):
-    annual_g = annual_growthFactors[i]
-    g = growthFactors[i]
-    NewEconomy = deepcopy(Economy)
-    
-    print('Now solving model for annual growth = ' + str(annual_g))
-    for j in range(len(NewEconomy.agents)):
-        NewEconomy.agents[j].PermGroFac = [g]
-    
-    t_start = clock()
-    NewEconomy.solve()
-    NewEconomy.calcLorenzDistance()
-    NewEconomy.showManyStats()
-    t_end = clock()
-    
-    LorenzLongLvlSim.append(NewEconomy.LorenzLongLvlSim)
-    LorenzLongNrmSim.append(NewEconomy.LorenzLongNrmSim)
-    LorenzLongIncSim.append(NewEconomy.LorenzLongIncSim)
-    aLvlGiniSim.append(getGiniPrc(NewEconomy.LorenzLongLvlSim))
-    aNrmGiniSim.append(getGiniPrc(NewEconomy.LorenzLongNrmSim))
-    IncGiniSim.append(getGiniPrc(NewEconomy.LorenzLongIncSim))
-    aLvlMeanSim.append(NewEconomy.aLvlMeanSim)
-    aNrmMeanSim.append(NewEconomy.aNrmMeanSim)
-    aLvlMedianSim.append(NewEconomy.aLvlMedianSim)
-    aNrmMedianSim.append(NewEconomy.aNrmMedianSim)
-    aLvlMeanToMedianSim.append(NewEconomy.aLvlMeanSim / NewEconomy.aLvlMedianSim)
-    aNrmMeanToMedianSim.append(NewEconomy.aNrmMeanSim / NewEconomy.aNrmMedianSim)
-    aLvlPercentilesSim.append(NewEconomy.aLvlPercentilesSim)
-    aNrmPercentilesSim.append(NewEconomy.aNrmPercentilesSim)
-    
-    print('Solving took ' + str(t_end-t_start) + ' seconds.\n')
+T_age_list = [400, 200, 160]
+
+pdb.set_trace()
+
+for T_age in T_age_list:
+    for i in range(len(growthFactors)):
+        annual_g = annual_growthFactors[i]
+        g = growthFactors[i]
+        NewEconomy = deepcopy(Economy)
+        
+        print('Now solving model for T_age = ' + str(T_age) + ' and annual growth = ' + str(annual_g))
+        for j in range(len(NewEconomy.agents)):
+            NewEconomy.agents[j].PermGroFac = [g]
+            NewEconomy.agents[j].T_age = T_age
+        
+        t_start = clock()
+        NewEconomy.solve()
+        NewEconomy.calcLorenzDistance()
+        NewEconomy.showManyStats()
+        t_end = clock()
+        
+        T_ageSim.append(T_age)
+        LorenzLongLvlSim.append(NewEconomy.LorenzLongLvlSim)
+        LorenzLongNrmSim.append(NewEconomy.LorenzLongNrmSim)
+        LorenzLongIncSim.append(NewEconomy.LorenzLongIncSim)
+        aLvlGiniSim.append(getGiniPrc(NewEconomy.LorenzLongLvlSim))
+        aNrmGiniSim.append(getGiniPrc(NewEconomy.LorenzLongNrmSim))
+        IncGiniSim.append(getGiniPrc(NewEconomy.LorenzLongIncSim))
+        aLvlMeanSim.append(NewEconomy.aLvlMeanSim)
+        aNrmMeanSim.append(NewEconomy.aNrmMeanSim)
+        aLvlMedianSim.append(NewEconomy.aLvlMedianSim)
+        aNrmMedianSim.append(NewEconomy.aNrmMedianSim)
+        aLvlMeanToMedianSim.append(NewEconomy.aLvlMeanSim / NewEconomy.aLvlMedianSim)
+        aNrmMeanToMedianSim.append(NewEconomy.aNrmMeanSim / NewEconomy.aNrmMedianSim)
+        aLvlPercentilesSim.append(NewEconomy.aLvlPercentilesSim)
+        aNrmPercentilesSim.append(NewEconomy.aNrmPercentilesSim)
+        
+        print('Solving took ' + str(t_end-t_start) + ' seconds.\n')
 
 
 # Save growth factors and corresponding results
 with open('../../output/VaryGrowth/' + Params.spec_name + '.pkl', 'w') as f:
-    pickle.dump([annual_growthFactors,
-                 growthFactors,
+    pickle.dump([T_ageSim,
+                 np.tile(annual_growthFactors, len(T_age_list)),
+                 np.tile(growthFactors, len(T_age_list)),
                  LorenzLongLvlSim,
                  LorenzLongNrmSim,
                  LorenzLongIncSim,
