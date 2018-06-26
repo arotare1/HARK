2a3ac24ea9eb8675b930706fe0e483b5dc51d72f
arotare1 <32108515+arotare1@users.noreply.github.com>
2018-06-25 22:46:09 -0400

    Make figure compareing cases when people update their consumption rule as growth changes and when they do not

---
+++ cstwGrowth/MakeFigures_no_update.R
@@ -0,0 +1,67 @@
+# =============================================================================================== #
+# Make figures comparing wealth inequality for the case when agents update their consumption rule 
+# as growth changes to the case when they don't. The difference between these two cases comes from 
+# the erosion of the wealth-to-income ratio due to the change in income growth.
+#
+# Saves output in ../../output/Figures_no_update/ and ../../tex/
+# =============================================================================================== #
+
+library(plyr)
+library(tidyverse)
+library(magrittr)
+library(haven)
+library(readxl)
+library(scales)
+library(ggplot2)
+library(ggrepel)
+library(reshape2)
+library(gridExtra)
+
+
+setwd("/Users/andreea/Documents/phd/2ndyrpaper/output/Figures_no_update")
+
+# Write function that extracts legend
+# https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+# Import model simulation results
+high_growth <- read.csv("../NoUpdate/HighGrowth/low_T_age_actual_KY/Dist.csv")
+no_growth <- read.csv("../NoUpdate/NoGrowth/low_T_age_actual_KY/Dist.csv")
+
+high_growth <- melt(high_growth, id.vars = "annual_growth")
+no_growth <- melt(no_growth, id.vars = "annual_growth")
+
+panel_a <- ggplot(high_growth %>% filter(variable=="Lvl_mean_to_median" |
+                                           variable=="Lvl_mean_to_median_no_update"),
+                  aes(x = annual_growth-1, y = value, color = variable)) +
+  geom_line() +
+  geom_point() +
+  scale_color_discrete(breaks = c("Lvl_mean_to_median", "Lvl_mean_to_median_no_update"),
+                       labels = c("update", "no update")) +
+  labs(x = "Growth rate", y = "Mean to median ratio", title = "(a) Wealth levels", color = "") +
+  theme_light() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 10),
+        axis.title = element_text(size = 9),
+        legend.position = "bottom")
+
+panel_b <- ggplot(high_growth %>% filter(variable=="Nrm_mean_to_median" |
+                                           variable=="Nrm_mean_to_median_no_update"),
+                  aes(x = annual_growth-1, y = value, color = variable)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Growth rate", y = "Mean to median ratio", title = "(b) Wealth to income ratios") +
+  theme_light() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 10),
+        axis.title = element_text(size = 9))
+
+update_vs_no_update <- grid.arrange(arrangeGrob(panel_a + theme(legend.position="none", plot.margin = unit(c(0.3,0.3,0.3,0.3), "cm")),
+                                                panel_b + theme(legend.position="none", plot.margin = unit(c(0.3,0.3,0.3,0.3), "cm")),
+                                                nrow = 1),
+                                    g_legend(panel_a), nrow = 2, heights = c(10,1))
+ggsave("update_vs_no_update.pdf", plot = update_vs_no_update, device = "pdf", width = 8, height = 4)
+ggsave("../../tex/update_vs_no_update.pdf", plot = update_vs_no_update, device = "pdf", width = 8, height = 4)
+
